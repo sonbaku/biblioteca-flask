41d1e53e81df7d5f68419f63fa46a44e02466313
sonbaku <marcosmartinez2319@gmail.com>
2026-03-15 19:26:48 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, request, redirect
+import psycopg2
+
+app = Flask(__name__)
+
+conexion = psycopg2.connect(
+    host="localhost",
+    database="biblioteca",
+    user="postgres",
+    password="0312"
+)
+
+cursor = conexion.cursor()
+
+
+@app.route('/')
+def login():
+    return render_template("login.html")
+@app.route('/login', methods=['POST'])
+def validar_login():
+
+    usuario = request.form['usuario']
+    password = request.form['password']
+
+    cursor.execute(
+        "SELECT * FROM usuarios WHERE usuario=%s AND password=%s",
+        (usuario, password)
+    )
+
+    user = cursor.fetchone()
+
+    if user:
+        return redirect('/menu')
+    else:
+        return render_template("login.html", error="Usuario o contraseña incorrectos")
+
+
+@app.route('/menu')
+def menu():
+    return render_template("menu.html")
+
+
+@app.route('/libros')
+def libros():
+
+    cursor.execute("SELECT * FROM libros")
+    libros = cursor.fetchall()
+
+    return render_template("libros.html", libros=libros)
+
+
+@app.route('/agregar')
+def agregar():
+    return render_template("agregar.html")
+
+
+@app.route('/guardar', methods=['POST'])
+def guardar():
+
+    titulo = request.form['titulo']
+    autor = request.form['autor']
+    anio = request.form['anio']
+    precio = request.form['precio']
+
+    cursor.execute(
+        "INSERT INTO libros (titulo, autor, anio, precio) VALUES (%s,%s,%s,%s)",
+        (titulo, autor, anio, precio)
+    )
+
+    conexion.commit()
+
+    return redirect('/libros')
+
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+
+    cursor.execute("DELETE FROM libros WHERE id = %s", (id,))
+    conexion.commit()
+
+    return redirect('/libros')
+
+@app.route('/editar/<int:id>')
+def editar(id):
+
+    cursor.execute("SELECT * FROM libros WHERE id = %s", (id,))
+    libro = cursor.fetchone()
+
+    return render_template("editar.html", libro=libro)
+
+@app.route('/actualizar/<int:id>', methods=['POST'])
+def actualizar(id):
+
+    titulo = request.form['titulo']
+    autor = request.form['autor']
+    anio = request.form['anio']
+    precio = request.form['precio']
+
+    cursor.execute(
+        "UPDATE libros SET titulo=%s, autor=%s, anio=%s, precio=%s WHERE id=%s",
+        (titulo, autor, anio, precio, id)
+    )
+
+    conexion.commit()
+
+    return redirect('/libros')
+@app.route('/logout')
+def logout():
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
